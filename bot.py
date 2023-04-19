8f8ee6cd132dc02f313c72f682ca8f6110547e14
Rudraa332 <rudraa2213@proton.mail>
2023-04-19 11:39:02 +0530

    H

--- bot.py
+++ bot.py
@@ -1,15 +1,19 @@
-import pytube 
+import pytube
 import os
 from config import Config
-from pyrogram import Client, filters,enums
-from pyrogram.types import (Message,InlineKeyboardMarkup,InlineKeyboardButton) 
+from pyrogram import Client, filters, enums
+from pyrogram.types import (
+    Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery)
 
 # Create a Pyrogram client object
 
 API_ID = Config.API_ID
 API_HASH = Config.API_HASH
 BOT_TOKEN = Config.BOT_TOKEN
-HOME_TEXT=Config.HOME_TEXT
+HOME_TEXT = Config.HOME_TEXT
+ABOUT_DEV_TEXT = Config.ABOUT_DEV_TEXT
+ABOUT_BOT_TEXT = Config.ABOUT_BOT_TEXT
+
 app = Client('my_bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 
@@ -17,46 +21,115 @@ app = Client('my_bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 @app.on_message(filters.command('start'))
 def start_handler(client, message):
     message.reply_text(
-          HOME_TEXT.format(message.from_user.first_name, message.from_user.id),
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(
+        HOME_TEXT.format(message.from_user.first_name, message.from_user.id),
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
                 [
-                    [
-                        InlineKeyboardButton("Support Group 🥺", url="https://telegram.me/Joint0T"),
-                        InlineKeyboardButton("Bots Channel 🙂", url="https://telegram.me/bot_channelv1")
-                    ],
-                    [
-                        InlineKeyboardButton("Get My Repo🙌", url="https://github.com/Rudraa332/Pytube-Telegram-Bot")
-                    ]
-                    # [
-                    #     InlineKeyboardButton("About Bot", callback_data="aboutbot"),
-                    #     InlineKeyboardButton("About Dev", callback_data="aboutdevs")
-                    # ]
+                    InlineKeyboardButton(
+                        "Support Group 🥺", url="https://telegram.me/Joint0T"),
+                    InlineKeyboardButton(
+                        "Bots Channel 🙂", url="https://telegram.me/bot_channelv1")
+                ],
+                [
+                    InlineKeyboardButton(
+                        "About Bot", callback_data="aboutbot"),
+                    InlineKeyboardButton(
+                        "About Dev", callback_data="aboutdevs")
                 ]
-            )
+            ]
+        )
     )
 
 # Define a handler function to respond to text messages containing a YouTube link
 # @app.on_message(filters.text & filters.private)
 # async def youtube_handler(client, message: Message):
-@app.on_message(filters.regex(".*(youtube.com/|youtu.be/).*") &filters.private)
+
+
+@app.on_message(filters.regex(".*(youtube.com/|youtu.be/).*") & filters.private)
 async def handle_message(client, message: Message):
-        # Create a YouTube object and get the highest resolution stream
-        yt = pytube.YouTube(message.text)
-        stream = yt.streams.get_highest_resolution()
-        await app.send_chat_action(chat_id=message.chat.id , action=enums.ChatAction.TYPING)
+    # Create a YouTube object and get the highest resolution stream
+    yt = pytube.YouTube(message.text)
+    stream = yt.streams.get_highest_resolution()
+    await app.send_chat_action(chat_id=message.chat.id, action=enums.ChatAction.TYPING)
 
-        # Download the video to the current working directory
-        file_path = stream.download()
-        await app.send_chat_action(chat_id=message.chat.id , action=enums.ChatAction.RECORD_VIDEO)
+    # Download the video to the current working directory
+    file_path = stream.download()
+    await app.send_chat_action(chat_id=message.chat.id, action=enums.ChatAction.RECORD_VIDEO)
 
+    # Display a "sending video" message
+    await app.send_chat_action(chat_id=message.chat.id, action=enums.ChatAction.UPLOAD_VIDEO)
 
-        # Display a "sending video" message
-        await app.send_chat_action(chat_id=message.chat.id , action=enums.ChatAction.UPLOAD_VIDEO)
-
-        # Send the video file to the user
-        video_file = await app.send_video(chat_id=message.chat.id, video=file_path, caption='Here is your video!')
-        # await app.send_document(chat_id=message.chat.id, document=video_file.video.file_id)
-        os.remove(file_path)
+    # Send the video file to the user
+    video_file = await app.send_video(chat_id=message.chat.id, video=file_path, caption='Here is your video!')
+    # await app.send_document(chat_id=message.chat.id, document=video_file.video.file_id)
+    os.remove(file_path)
 # Start the Pyrogram client
+
+
+@app.on_callback_query()
+async def button(client, cmd: CallbackQuery):
+    cb_data = cmd.data
+    if "aboutbot" in cb_data:
+        await cmd.message.edit(
+            ABOUT_DEV_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Get My Repo🙌",
+                                             url="https://github.com/Rudraa332/Pytube-Telegram-Bot")
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "Go Home", callback_data="gotohome"),
+                        InlineKeyboardButton(
+                            "About Dev", callback_data="aboutdevs")
+                    ]
+                ]
+            )
+        )
+
+    elif "aboutdevs" in cb_data:
+        await cmd.message.edit(
+            ABOUT_DEV_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Get My Repo🙌",
+                                             url="https://github.com/Rudraa332/Pytube-Telegram-Bot")
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "About Bot", callback_data="aboutbot"),
+                        InlineKeyboardButton(
+                            "Go Home", callback_data="gotohome")
+                    ]
+                ]
+            )
+        )
+
+    elif "gotohome" in cb_data:
+        await cmd.message.edit(
+            HOME_TEXT.format(cmd.message.chat.first_name, cmd.message.chat.id),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "Support Group", url="https://telegram.me/Joint0T"),
+                        InlineKeyboardButton(
+                            "Bots Channel", url="https://telegram.me/bot_channelv1")
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "About Bot", callback_data="aboutbot"),
+                        InlineKeyboardButton(
+                            "About Dev", callback_data="aboutdevs")
+                    ]
+                ]
+            )
+        )
+
 app.run()
--- config.py
+++ config.py
@@ -2,11 +2,40 @@
 import os
 
 class Config(object):
+    BOT_USERNAME = os.environ.get("BOT_USERNAME",'link_Short_dagd_bot')
     API_ID = int(os.environ.get("API_ID", ""))
     API_HASH = os.environ.get("API_HASH", '')
     BOT_TOKEN = os.environ.get("BOT_TOEKEN", '')
     HOME_TEXT = """
 Hi, [{}](tg://user?id={})\n\nI Am **PyTube Bot**.
 
-I'm A Youtube Video Downloader Bot In High Resolution\nSend Me A link of Youtube And I will Send Your Video Thank You!. I Also have Support Channel ! Please Check **About Bot** Button.
+I'm A Youtube Video Downloader Bot In High Resolution\nSend Me A link of Youtube And I will Send Your The Video Thank You. I Also have Support Channel ! Please Check **About Bot** Button.
+"""
+    ABOUT_DEV_TEXT = f"""
+🧑🏻‍💻 **Developer:** @Rudraa332
+
+Developer is Excellent. And Learning from Official docs and ChatGpt. if you want to know about the developer the You can Dm Me.
+I Am A Simple Quick learner Student if you want to teach me Please help Me to Improve myself.
+[Rudraa](https://www.telegram.me/rudraa332) (Contect Me)
+"""
+    ABOUT_BOT_TEXT=f""" 
+
+    I Am **Rudraa's** PyTube bot! \n I am simple youtube video Downloader bot. Send me a youtube link and I will give you th video of link!😊
+
+
+    🤖 **My Name:** [Pytube Bot](https://t.me/{BOT_USERNAME})
+
+    📝 **Language:** [Python3](https://www.python.org/)
+
+    📚 **Library1:** [Pyrogram](https://docs.pyrogram.org/)
+
+    📚 **Library2:** [PyTube](https://pypi.org/project/pytube/)
+
+    📡 **Hosted on:** [Railway](https://render.com/)
+
+    🧑🏻‍💻 **Developer:** @Rudraa332
+
+    👥 **Support Group:** [Support Group](https://t.me/joint0t)
+
+    📢 **Updates Channel:** [Bot Channel](https://t.me/bot_channelv1)
 """
